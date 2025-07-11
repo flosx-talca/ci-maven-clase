8d9db92c035630811a2427aedc8f091e649e3aa2
sebastian <sebastian@boolean.cl>
2025-07-10 20:12:55 -0400

    JUnit5 working

---
+++ src/test/java/com/devopsgang/AppTest.java
@@ -0,0 +1,13 @@
+package com.devopsgang;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true, "El test básico debe pasar");
+    }
+}
