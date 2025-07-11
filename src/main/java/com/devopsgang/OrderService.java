ddc93c55bc8286efbbae673313cc8738748d9e18
sebastian <sebastian@boolean.cl>
2025-07-10 21:40:06 -0400

    TestMocks

--- src/main/java/com/devopsgang/OrderService.java
+++ src/main/java/com/devopsgang/OrderService.java
@@ -1,13 +1,18 @@
 package com.devopsgang;
 
 public class OrderService{
-  public double getTotal(double subtotal, boolean hasDiscount, boolean hasExpressShipment){
+  private DiscountService discountService;
+  public OrderService(DiscountService discountService){
+    this.discountService = discountService;
+  }
+
+  public double getTotal(double subtotal, String discountCode, boolean hasExpressShipment){
     if (subtotal < 0) {
         throw new IllegalArgumentException("Subtotal no puede ser negativo");
     }
     
     
-    double discount = hasDiscount ? 0.10 : 0;
+    double discount = discountService.getRate(discountCode);
     double shipment = hasExpressShipment ? 20.0 : 10.0;
 
     return (subtotal * (1-discount)) + shipment;
